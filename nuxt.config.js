fc8a2d22dc4cea86b75c050b29b012ad139f5f43
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 00:15:57 +0900

    :wrench: yarn add @nuxtjs/composition-api #3

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
