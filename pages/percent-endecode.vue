6e20cdd02dc2a90e3beaaef58ccb4856376044ef
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 01:40:15 +0900

    :wrench: ラジオボタン切り替えでテキストエリアのクリア

--- pages/percent-endecode.vue
+++ pages/percent-endecode.vue
@@ -3,14 +3,37 @@
     <h2 class="text-xl font-extrabold pt-4">パーセントエンコード/デコードするやつ</h2>
     <div class="divider"></div>
     <div class="flex items-center pb-4">
-      <input type="radio" name="radio-1" id="hoge" class="radio radio-primary" checked="checked" />
-      <label for="hoge" class="label cursor-pointer pr-4">エンコード</label>
-      <input type="radio" name="radio-1" id="fuga" class="radio radio-primary" />
-      <label for="fuga" class="label cursor-pointer">デコード</label>
+      <input type="radio" name="option" id="encode" class="radio radio-primary" value="encode" v-model="option" @change="onChange"/>
+      <label for="encode" class="label cursor-pointer pr-4">エンコード</label>
+      <input type="radio" name="option" id="decode" class="radio radio-primary" value="decode" v-model="option" @change="onChange"/>
+      <label for="decode" class="label cursor-pointer">デコード</label>
     </div>
-    <textarea class="textarea textarea-primary textarea-md w-full h-32 mb-4" placeholder="入力"></textarea>
-    <textarea class="textarea textarea-primary textarea-md w-full h-32" placeholder="出力"></textarea>
+    <textarea class="textarea textarea-primary textarea-md w-full h-32 mb-4" placeholder="入力" v-model="inputText"></textarea>
+    <textarea class="textarea textarea-primary textarea-md w-full h-32" placeholder="出力" v-model="outputText"></textarea>
     <button class="btn btn-primary w-40 mt-4 mb-4">実行</button>
     <button class="btn btn-secondary w-40 mt-4 mb-4">結果をコピー</button>
   </div>
 </template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const option = ref('encode');
+    const inputText = ref('');
+    const outputText = ref('');
+
+    const onChange = (() => {
+      inputText.value = '';
+      outputText.value = '';
+    });
+    return {
+      option,
+      inputText,
+      outputText,
+      onChange,
+    }
+  },
+})
+</script>
