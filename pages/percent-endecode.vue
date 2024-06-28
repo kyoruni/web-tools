553ceefbad2e49de8476a28097fcadb088bcd5be
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 01:20:27 +0900

    :lipstick: 見た目の作成

--- pages/percent-endecode.vue
+++ pages/percent-endecode.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="percent-endecode">
-    percent
+    <h2 class="text-xl font-extrabold">パーセントエンコード/デコードするやつ</h2>
+    <div class="divider"></div>
+    <div class="flex items-center pb-4">
+      <input type="radio" name="radio-1" id="hoge" class="radio radio-primary" checked="checked" />
+      <label for="hoge" class="label cursor-pointer pr-4">エンコード</label>
+      <input type="radio" name="radio-1" id="fuga" class="radio radio-primary" />
+      <label for="fuga" class="label cursor-pointer">デコード</label>
+    </div>
+    <textarea class="textarea textarea-primary textarea-md w-full h-32 mb-4" placeholder="入力"></textarea>
+    <textarea class="textarea textarea-primary textarea-md w-full h-32" placeholder="出力"></textarea>
+    <button class="btn btn-primary w-40 mt-4 mb-4">実行</button>
+    <button class="btn btn-secondary w-40 mt-4 mb-4">結果をコピー</button>
   </div>
 </template>
