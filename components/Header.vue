604545bc95b4f78982e59386b01baf8c07889738
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-28 23:45:43 +0900

    :lipstick: アイコンを表示してみる #3

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar bg-primary text-primary-content">
     <button class="btn btn-ghost text-xl">
+      <Icon />
       <nuxt-link to="/">
         kyoruni/web-tools
       </nuxt-link>
