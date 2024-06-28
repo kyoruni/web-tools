410b99ffe071b6487d4731d81b1c7db5f7460065
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-28 22:31:42 +0900

    :lipstick: ちょっとパディング入れる #3

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,7 @@
     <div class="flex min-h-screen flex-col md:flex-row">
       <Menu class="w-full md:w-1/4" style="background: pink;" />
       <div class="content w-full md:w-3/4">
-        <nuxt />
+        <nuxt class="p-2" />
       </div>
     </div>
   </div>
