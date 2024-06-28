3b3ae0e502156f6b34c904793cfb69f3fa3ce060
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-28 22:30:22 +0900

    :lipstick: ページの切り替え #3

---
+++ pages/fuga.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>fuga</p>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="web-tools" data-theme="cupcake">
-    <div class="navbar bg-primary text-primary-content">
-      <button class="btn btn-ghost text-xl">kyoruni/web-tools</button>
-    </div>
-    <div class="flex min-h-screen flex-col md:flex-row">
-      <Menu class="w-full md:w-1/4" style="background: pink;" />
-      <div class="content w-full md:w-3/4">
-        <Tutorial />
-      </div>
-    </div>
-  </div>
+  <Tutorial />
 </template>
 
 <script lang="ts">
