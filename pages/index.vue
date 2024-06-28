b9c46a0ff4809637eba1d637e057440e6cf61530
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-28 22:18:41 +0900

    :lipstick: 画面幅が小さくなったら1カラムにする #3

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,11 @@
     <div class="navbar bg-primary text-primary-content">
       <button class="btn btn-ghost text-xl">kyoruni/web-tools</button>
     </div>
-    <div class="container flex min-h-screen">
-      <Menu class="w-1/4" style="background: pink;" />
-      <Tutorial />
+    <div class="flex min-h-screen flex-col md:flex-row">
+      <Menu class="w-full md:w-1/4" style="background: pink;" />
+      <div class="content w-full md:w-3/4">
+        <Tutorial />
+      </div>
     </div>
   </div>
 </template>
