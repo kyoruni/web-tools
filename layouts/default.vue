0ae154530c58259c6e69d44d67b7764598b6260a
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 20:12:40 +0900

    :wrench: デバッグ用のスタイル削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <Header />
     <!-- コンテンツ -->
     <div class="flex min-h-screen flex-col md:flex-row">
-      <Menu class="w-full md:w-1/4" style="background: pink;" />
+      <Menu class="w-full md:w-1/4" />
       <div class="content w-full md:w-3/4">
         <div class="p-2">
           <slot />
