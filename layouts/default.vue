46dbc3b77408c17847df0227a57ba16762a586f1
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 21:22:37 +0900

    :lipstick: 余白の調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <div class="flex min-h-screen flex-col md:flex-row">
       <Menu class="w-full md:w-1/4" />
       <div class="content w-full md:w-3/4">
-        <div class="p-2">
+        <div class="p-2 pt-4">
           <slot />
         </div>
       </div>
