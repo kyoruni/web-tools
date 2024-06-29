99ce4cddfb7f4012481da15772560fe8bbc12d3f
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 19:48:52 +0900

    :wrench: daisyUIを導入

--- app.vue
+++ app.vue
@@ -2,6 +2,13 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       Button
     </button>
