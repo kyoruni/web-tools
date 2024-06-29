dce0fd50827502d6e2ab32bc5fb74434f5cb32ac
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 22:27:49 +0900

    GitHub Pagesで公開してみる (#7)
    * :wrench: ベースURL設定
    * :wrench: このファイルがないと、うまくビルドできないらしい
    * :wrench: ワークフローの作成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/web-tools/',
     head: {
       title: "kyoruni | web-tools",
       meta: [
