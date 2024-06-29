c7d28e356706872b19f44007db28ec24818f825e
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 21:54:20 +0900

    :wrench: 実行ボタンは常に押下可能にしておく

--- pages/percent-endecode.vue
+++ pages/percent-endecode.vue
@@ -10,7 +10,7 @@
     </div>
     <textarea class="textarea textarea-primary textarea-md w-full h-32 mb-4" placeholder="入力" v-model="inputText"></textarea>
     <textarea class="textarea textarea-primary textarea-md w-full h-32" placeholder="出力" v-model="outputText"></textarea>
-    <button class="btn btn-primary w-40 mt-4 mb-4" :class="{ 'btn-disabled': isInputEmpty }" :aria-disabled="isInputEmpty" @click="execute">実行</button>
+    <button class="btn btn-primary w-40 mt-4 mb-4" @click="execute">実行</button>
     <button class="btn btn-secondary w-40 mt-4 mb-4" :class="{ 'btn-disabled': isOutputEmpty }" :aria-disabled="isOutputEmpty" @click="copyButton">結果をコピー</button>
   </div>
 </template>
@@ -39,6 +39,12 @@ const onChange = (() => {
 });
 
 const execute = (() => {
+  if (inputText.value.length === 0) {
+    const optionKind = option.value === 'encode' ? 'エンコード' : 'デコード';
+    alert(`${optionKind}したいテキストを入力してください！`);
+    return;
+  }
+
   if (option.value === 'encode') {
     outputText.value = encodeURIComponent(inputText.value);
   } else {
