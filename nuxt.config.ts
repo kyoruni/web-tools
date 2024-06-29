3066dec0efc3c0e89179be6c90ade838f2cc0393
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 20:12:00 +0900

    :wrench: ファビコン差し替え、ページタイトル設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "kyoruni | web-tools",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    }
+  },
   devtools: { enabled: true },
   devServer: {
     port: 7777
