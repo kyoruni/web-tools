51f4802449bcb3cec1915cbc31e41d95de6baef3
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 20:06:59 +0900

    :lipstick: ヘッダーをコンポーネントに切り出す

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <button class="btn btn-ghost text-xl">
+      <nuxt-link to="/">
+        kyoruni/web-tools
+      </nuxt-link>
+    </button>
+  </div>
+</template>
