12d534d0483904276e217652a27c3bf606b0fc0f
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 20:05:59 +0900

    :lipstick: ちょっとパディング入れる

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,9 @@
     <div class="flex min-h-screen flex-col md:flex-row">
       <Menu class="w-full md:w-1/4" style="background: pink;" />
       <div class="content w-full md:w-3/4">
-        <slot />
+        <div class="p-2">
+          <slot />
+        </div>
       </div>
     </div>
   </div>
