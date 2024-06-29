75e697b06d3a62e9b4951066ad72d138870b5750
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 21:49:19 +0900

    :wrench: 出力があるときだけ、コピーボタンを押せるように

--- pages/percent-endecode.vue
+++ pages/percent-endecode.vue
@@ -11,12 +11,12 @@
     <textarea class="textarea textarea-primary textarea-md w-full h-32 mb-4" placeholder="入力" v-model="inputText"></textarea>
     <textarea class="textarea textarea-primary textarea-md w-full h-32" placeholder="出力" v-model="outputText"></textarea>
     <button class="btn btn-primary w-40 mt-4 mb-4">実行</button>
-    <button class="btn btn-secondary w-40 mt-4 mb-4" @click="copyButton">結果をコピー</button>
+    <button class="btn btn-secondary w-40 mt-4 mb-4" :class="{ 'btn-disabled': isOutputEmpty }" :aria-disabled="isOutputEmpty" @click="copyButton">結果をコピー</button>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useClipboard } from '@vueuse/core';
 
 const { copy, copied } = useClipboard();
@@ -24,6 +24,10 @@ const { copy, copied } = useClipboard();
 const option = ref('encode');
 const inputText = ref('');
 const outputText = ref('');
+
+const isOutputEmpty = computed(() => {
+  return outputText.value.length === 0;
+});
 
 const onChange = (() => {
   inputText.value = '';
