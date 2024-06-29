1ddc85883496d000674ae9d2da42c47579d8fac0
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 19:54:23 +0900

    :recycle: 不要なクラス削除

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="menu">
-    <ul class="menu bg-base-200 rounded-box w-full">
+    <ul class="menu bg-base-200 rounded-box">
       <li><a>Item 1</a></li>
       <li><a>Item 2</a></li>
       <li><a>Item 3</a></li>
