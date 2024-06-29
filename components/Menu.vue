9d8f603eb86bf8ef116e276a4b3acb9b0254fbff
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-06-29 22:07:06 +0900

    :heavy_minus_sign: デバッグ用のページを削除

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,9 +1,6 @@
 <template>
   <nav class="menu">
     <ul class="menu bg-base-200 rounded-box">
-      <li><nuxt-link to="/">indexindexindex</nuxt-link></li>
-      <li><nuxt-link to="/hoge">hogehogehoge</nuxt-link></li>
-      <li><nuxt-link to="/fuga">fugafugafuga</nuxt-link></li>
       <li><nuxt-link to="/percent-endecode">パーセントエンコード/デコードするやつ</nuxt-link></li>
     </ul>
   </nav>
--- pages/hoge.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="hoge">
-    <Icon :kind="'star'"/>
-    <p>hoge</p>
-  </div>
-</template>
