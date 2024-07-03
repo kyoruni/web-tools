f1313a5acbf91c73bcfd1a557d4213aadaf0bc03
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-03 21:35:33 +0900

    :recycle: タイトル部分を別コンポーネントに切り出し

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="menu">
     <ul class="menu bg-base-200 rounded-box">
-      <li><nuxt-link to="/percent-endecode">パーセントエンコード/デコードするやつ</nuxt-link></li>
+      <li><nuxt-link to="/percent-endecode">パーセントエンコード・デコードするやつ</nuxt-link></li>
     </ul>
   </nav>
 </template>
---
+++ components/PageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-title">
+    <h2 class="text-xl font-extrabold pt-4">{{ props.value }}</h2>
+    <div class="divider"></div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: {
+    type: String,
+    required: true,
+  }
+});
+</script>
