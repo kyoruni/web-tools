73b8dc5689f4ecf4bae7c7b779f1bed0ea0cfc3d
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-04 21:02:41 +0900

    :wrench: ページ作成

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,6 +2,7 @@
   <nav class="menu">
     <ul class="menu bg-base-200 rounded-box">
       <li><nuxt-link to="/percent-endecode">パーセントエンコード・デコードするやつ</nuxt-link></li>
+      <li><nuxt-link to="/string-generator">指定した字数の文字を作るやつ</nuxt-link></li>
     </ul>
   </nav>
 </template>
---
+++ pages/string-generator.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="string-generator">
+    <PageTitle :value="'指定した字数の文字を作るやつ'" />
+  </div>
+</template>
