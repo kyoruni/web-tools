3c700fef563a8fe0d433a2d1a6568db5e6256da7
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-04 21:31:00 +0900

    :wrench: セレクトボックスの値を変更できるように

--- pages/string-generator.vue
+++ pages/string-generator.vue
@@ -8,20 +8,33 @@
       <label for="full" :class="labelClass">全角</label>
       ({{ widthType }})
     </div>
-    <select class="select select-primary w-full max-w-xs rounded">
+    <select class="select select-primary w-full max-w-xs rounded" @change="selectKind">
       <option disabled selected>文字の種類を選択</option>
-      <option>aaaaa</option>
-      <option>bbbbb</option>
-      <option>ccccc</option>
-      <option>ddddd</option>
+      <option v-for="kind in kinds" :key="kind.value" :value="kind.value">
+        {{ kind.displayName }}
+      </option>
     </select>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 const widthType = ref('half'); // half or full
+const charType = ref('number');
+const kinds = ref([]);
 
 const radioClass = 'radio radio-primary';
 const labelClass = 'label cursor-pointer';
+
+const selectKind = ((event) => {
+  charType.value = event.target.value;
+});
+
+onMounted(() => {
+  kinds.value = [
+    { displayName: '数字', value: 'number' },
+    { displayName: '英字(大文字)', value: 'upperAlpha' },
+    { displayName: '英字(小文字)', value: 'lowerAlpha' },
+  ];
+});
 </script>
