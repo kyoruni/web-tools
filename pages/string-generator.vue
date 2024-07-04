b958e6afb380823537d00cde3a18fbb8ef2661af
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-04 21:15:27 +0900

    :wrench: 接頭語いらない気がしたので削除

--- pages/string-generator.vue
+++ pages/string-generator.vue
@@ -2,11 +2,11 @@
   <div class="string-generator">
     <PageTitle :value="'指定した字数の文字を作るやつ'" />
     <div class="flex items-center pb-4">
-      <input type="radio" name="charWidthType" id="half" :class="radioClass" value="half" v-model="charWidthType" />
+      <input type="radio" name="widthType" id="half" :class="radioClass" value="half" v-model="widthType" />
       <label for="half" :class="labelClass" class="pr-4">半角</label>
-      <input type="radio" name="charWidthType" id="full" :class="radioClass" value="full" v-model="charWidthType" />
+      <input type="radio" name="widthType" id="full" :class="radioClass" value="full" v-model="widthType" />
       <label for="full" :class="labelClass">全角</label>
-      ({{ charWidthType }})
+      ({{ widthType }})
     </div>
     <select class="select select-primary w-full max-w-xs rounded">
       <option disabled selected>文字の種類を選択</option>
@@ -20,7 +20,7 @@
 
 <script setup>
 import { ref } from 'vue';
-const charWidthType = ref('half'); // half or full
+const widthType = ref('half'); // half or full
 
 const radioClass = 'radio radio-primary';
 const labelClass = 'label cursor-pointer';
