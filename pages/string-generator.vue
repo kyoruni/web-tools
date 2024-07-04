7d6e67776b72ed5137fdda80d0e55c18b62d6804
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-04 21:11:17 +0900

    :wrench: 半角or全角の選択

--- pages/string-generator.vue
+++ pages/string-generator.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="string-generator">
     <PageTitle :value="'指定した字数の文字を作るやつ'" />
+    <div class="flex items-center pb-4">
+      <input type="radio" name="charWidthType" id="half" :class="radioClass" value="half" v-model="charWidthType" />
+      <label for="half" :class="labelClass" class="pr-4">半角</label>
+      <input type="radio" name="charWidthType" id="full" :class="radioClass" value="full" v-model="charWidthType" />
+      <label for="full" :class="labelClass">全角</label>
+      ({{ charWidthType }})
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+const charWidthType = ref('half'); // half or full
+
+const radioClass = 'radio radio-primary';
+const labelClass = 'label cursor-pointer';
+</script>
