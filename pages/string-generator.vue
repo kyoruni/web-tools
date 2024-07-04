ec72d90784e5cd26b7faabae1c73cbf7c37593a4
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-04 22:41:19 +0900

    :wrench: 数字を生成できるように

--- pages/string-generator.vue
+++ pages/string-generator.vue
@@ -5,6 +5,7 @@
     <p>charType: {{ charType }}</p>
     <p>kinds: {{ kinds }}</p>
     <p>charCount: {{ charCount }}</p>
+    <p>isHalf: {{ isHalf }}</p>
     <div class="flex items-center pb-4">
       <input type="radio" name="widthType" id="half" :class="radioClass" value="half" v-model="widthType" @change="selectHalf" />
       <label for="half" :class="labelClass" class="pr-4">半角</label>
@@ -25,11 +26,14 @@
         v-model="charCount"/>
     </div>
     <textarea class="textarea textarea-primary textarea-md w-full h-32 rounded mt-4 mb-4" placeholder="ここに出力されます" v-model="outputText"></textarea>
+    <button class="btn btn-primary mr-2" :class="[isInvalidExecute ? 'btn-disabled' : '', buttonClass]" @click="execute">実行</button>
+    <button class="btn btn-secondary mr-2" :class="buttonClass">結果をコピー</button>
+    <button class="btn btn-accent" :class="buttonClass">クリア</button>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 const widthType = ref('half'); // half or full
 const charType = ref('');
 const kinds = ref([]);
@@ -38,6 +42,44 @@ const outputText = ref('');
 
 const radioClass = 'radio radio-primary';
 const labelClass = 'label cursor-pointer';
+const buttonClass = 'w-40 mt-4';
+
+const isInvalidExecute = computed(() => {
+  return !charType.value || charCount.value === 0;
+});
+
+const isHalf = computed(() => {
+  return widthType.value === 'half';
+});
+
+const execute = (() => {
+  switch (charType.value) {
+    case 'number':
+      generateNumber();
+      break;
+    default:
+      alert('作成中');
+  }
+});
+
+const generateNumber = (() => {
+  let chars = '';
+  if (isHalf.value) {
+    chars = '0123456789';
+  } else {
+    chars = '０１２３４５６７８９';
+  };
+  outputText.value = generate(chars, charCount.value);
+});
+
+const generate = (chars, length) => {
+  let result = '';
+  const charsLength = chars.length;
+  for (let i = 0; i < length; i++) {
+    result += chars[i % charsLength];
+  }
+  return result;
+};
 
 const selectHalf = (() => {
   charType.value = '';
