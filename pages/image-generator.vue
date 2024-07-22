fdaf691e7fa1356b9a6f5d7614098688d1191334
kyoruni <40832190+kyoruni@users.noreply.github.com>
2024-07-22 21:47:42 +0900

    :wrench: 入力欄を設置

--- pages/image-generator.vue
+++ pages/image-generator.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="image-generator">
     <PageTitle :value="'指定した大きさの画像を作るやつ'" />
+    <div class="flex">
+      <div class="form-control">
+        <div class="label">
+          <span class="label-text">幅(px)</span>
+        </div>
+        <input
+          type="number"
+          class="input input-bordered input-primary block w-full max-w-40 rounded ml-2"
+          v-model="width"/>
+      </div>
+      <div class="form-control">
+        <div class="label">
+          <span class="label-text">高さ(px)</span>
+        </div>
+        <input
+          type="number"
+          class="input input-bordered input-primary block w-full max-w-40 rounded ml-2"
+          v-model="height"/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const width = ref(300);
+const height = ref(300);
+</script>
