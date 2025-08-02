24bd2c8bdb856b1f2147096c3885f61ef75affb1
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-08-02 23:14:33 +0900

    指定した大きさの画像を作るやつ追加 (#12)
    * :wrench: ページ作成
    
    * :wrench: 入力欄を設置
    
    * :wrench: ボタンを設置
    
    * :wrench: 画像を生成
    
    * :wrench: 画像にテキストを表示
    
    * :wrench: クリアボタンの処理
    
    * :bug: 404が出ていた
    
    * :wrench: yarn add @ckpack/vue-color@^1.6.0
    
    * :construction: 画像の色を変更できるように
    
    * :lipstick: 画像背景色を選択できるように
    
    * :wrench: 文字色を変更できるように
    
    * :wrench: 画像はリアルタイムに変更
    
    * :bug: デバッグ用の記述が残っていた
    
    * :wrench: 画像のダウンロードボタン設置

---
+++ pages/image-generator.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="image-generator">
+    <PageTitle :value="'指定した大きさの画像を作るやつ'" />
+    <!-- 1段目 -->
+    <div class="flex">
+      <!-- 幅 -->
+      <div class="form-control pr-2">
+        <div class="label">
+          <span class="label-text">幅(px)</span>
+        </div>
+        <input
+          type="number"
+          class="input input-bordered input-primary block w-full max-w-40 rounded ml-2"
+          v-model="width"/>
+      </div>
+      <!-- 高さ -->
+      <div class="form-control">
+        <div class="label">
+          <span class="label-text">高さ(px)</span>
+        </div>
+        <input
+          type="number"
+          class="input input-bordered input-primary block w-full max-w-40 rounded ml-2"
+          v-model="height"/>
+      </div>
+    </div>
+    <!-- 2段目 -->
+    <div class="flex mt-2">
+      <!-- 背景色 -->
+      <div class="form-control">
+        <div class="label mb-2">
+          <span class="label-text">背景色</span>
+        </div>
+        <Twitter v-model="currentBackgroundColor"/>
+      </div>
+      <!-- 文字色 -->
+      <div class="form-control ml-4">
+        <div class="label mb-2">
+          <span class="label-text">文字色</span>
+        </div>
+        <Twitter v-model="currentFontColor"/>
+      </div>
+    </div>
+    <!-- ダウンロードボタン -->
+    <a :href="imageUrl" :download="fileName" class="btn btn-primary mt-4 mr-2 w-40">画像を保存</a>
+    <!-- 出力画像 -->
+    <div class="mt-4" v-if="imageUrl">
+      <img :src="imageUrl" :alt="`${width}x${height}`"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref, watch, onMounted } from 'vue';
+import { Twitter } from '@ckpack/vue-color';
+
+const width = ref(300);
+const height = ref(300);
+const imageUrl = ref('');
+const currentBackgroundColor = ref({hex: '#0000FF'});
+const currentFontColor = ref({hex: '#FFA500'});
+
+const fileName = computed(() => {
+  return `width_${width.value}_and_height_${height.value}.png`;
+});
+
+const generateImage = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width.value;
+  canvas.height = height.value;
+  const context = canvas.getContext('2d');
+
+  context.fillStyle = currentBackgroundColor.value.hex;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.font = '20px Arial';
+  context.fillStyle = currentFontColor.value.hex;
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText(`${width.value} × ${height.value}`, canvas.width / 2, canvas.height / 2);
+
+  imageUrl.value = canvas.toDataURL('image/png');
+}
+
+onMounted(() => {
+  generateImage();
+});
+
+watch([width, height, currentBackgroundColor, currentFontColor], () => {
+  generateImage();
+});
+</script>
